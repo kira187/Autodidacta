ffd11c13570b11264aca00278fb14596f660c1a9
Misael Aguas Jimenez <missael133@gmail.com>
2020-12-23 17:39:26 -0600

    HF: Correction to refernce model Course

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\course;
+use App\Models\Course;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        $courses = course::whereStatus(3)->latest('id')->limit(12)->get();
+        $courses = Course::whereStatus(3)->latest('id')->limit(12)->get();
 
         return view('welcome', compact('courses'));
     }
