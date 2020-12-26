b804af9599c8d39d976fbb68580de85ef3efb53b
Misael Aguas Jimenez <missael133@gmail.com>
2020-12-26 16:06:35 -0600

    Fix: changed position of close loop

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -28,30 +28,30 @@ class CourseSeeder extends Seeder
                 'imageable_id' => $course->id,
                 'imageable_type' => 'App\Models\Course'
             ]);
-        }
 
-        Requirement::factory(4)->create([
-            'course_id' => $course->id
-        ]);
+            Requirement::factory(4)->create([
+                'course_id' => $course->id
+            ]);
 
-        Audience::factory(4)->create([
-            'course_id' => $course->id
-        ]);
+            Audience::factory(4)->create([
+                'course_id' => $course->id
+            ]);
 
-        Goal::factory(4)->create([
-            'course_id' => $course->id
-        ]);
+            Goal::factory(4)->create([
+                'course_id' => $course->id
+            ]);
 
-        $sections = Section::factory(4)->create([
-            'course_id' => $course->id
-        ]);
+            $sections = Section::factory(4)->create([
+                'course_id' => $course->id
+            ]);
 
 
-        foreach ($sections as $section) {
-            $lessons = Lesson::factory(4)->create(['section_id' => $section->id]);
+            foreach ($sections as $section) {
+                $lessons = Lesson::factory(4)->create(['section_id' => $section->id]);
 
-            foreach ($lessons as $lesson) {
-                Description::factory(1)->create(['lesson_id' => $lesson->id]);
+                foreach ($lessons as $lesson) {
+                    Description::factory(1)->create(['lesson_id' => $lesson->id]);
+                }
             }
         }
     }
