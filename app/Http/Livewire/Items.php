d37372be63816d82b3cc344010e1dbef4b87b65e
Misael Aguas Jimenez <missael133@gmail.com>
2020-12-28 19:57:25 -0600

    Implement smetho sortBy

--- app/Http/Livewire/Items.php
+++ app/Http/Livewire/Items.php
@@ -9,12 +9,14 @@ use Livewire\WithPagination;
 class Items extends Component
 {
     use WithPagination;
-    public $active, $q;
+    public $active, $q, $sortBy = 'id', $sortAsc = true;
 
     //Muestra los queryes aplicados en la url
     protected $queryString = [
         'active' => ['except' => false],
-        'q' => ['except' => '']
+        'q' => ['except' => ''],
+        'sortBy' => ['except' => 'id'],
+        'sortAsc' => ['except' => true],
     ];
 
     public function render()
@@ -28,7 +30,8 @@ class Items extends Component
                 })
                 ->when($this->active, function( $query ) {
                     return $query->active();
-                });
+                })
+                ->orderBy( $this->sortBy, $this->sortAsc ? 'ASC' : 'DESC' );
         
         $query = $items->toSql();
         $items = $items->paginate(10);
@@ -46,5 +49,13 @@ class Items extends Component
     public function updatingQ()
     {
         $this->resetPage();
+    }
+
+    public function sortBy($field)
+    {
+        if ($field == $this->sortBy) {
+            $this->sortAsc =! $this->sortAsc;
+        }
+        $this->sortBy = $field;
     }
 }
