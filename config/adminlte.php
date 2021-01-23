d76a4ac5d3c471232f96c5f6da77712c6c05f846
kira187 Desktop <missael133@gmail.com>
2021-01-23 01:13:11 -0600

    Implement CRUD categories

--- config/adminlte.php
+++ config/adminlte.php
@@ -268,64 +268,25 @@ return [
             'icon' => 'fas fa-fw fa-user',
         ],
         [
-            'text' => 'change_password',
-            'url'  => 'admin/settings',
+            'text' => 'Categorias',
+            'route'  => 'admin.categories.index',
+            'icon' => 'fas fa-fw fa-cogs',
+        ],
+        [
+            'text' => 'Niveles de cursos',
+            'route'  => 'admin.levels.index',
+            'icon' => 'fas fa-fw fa-chart-line',
+        ],
+        [
+            'text' => 'Precios',
+            'route'  => 'admin.prices.index',
+            'icon' => 'fab fa-fw fa-cc-visa',
+        ],
+        [
+            'text' => 'Administrar cuenta',
+            'route'  => 'profile.show',
             'icon' => 'fas fa-fw fa-lock',
-        ],
-        [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-            ],
-        ],
-        ['header' => 'labels'],
-        [
-            'text'       => 'important',
-            'icon_color' => 'red',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'information',
-            'icon_color' => 'cyan',
-            'url'        => '#',
-        ],
+        ],        
     ],
 
     /*
--- routes/admin.php
+++ routes/admin.php
@@ -5,10 +5,16 @@ use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\CourseController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\LevelController;
+use App\Http\Controllers\Admin\PriceController;
 
 Route::get('/', [HomeController::class, 'index'])->middleware('can:Ver dashboard')->name('home');
 Route::resource('roles', Rolecontroller::class)->names('roles');
 Route::resource('users', UserController::class)->only('index', 'edit', 'update')->names('users');
+Route::resource('categories', CategoryController::class)->except(['show'])->names('categories');
+Route::resource('levels', LevelController::class)->except(['show'])->names('levels');
+Route::resource('prices', PriceController::class)->except(['show'])->names('prices');
 Route::get('courses', [CourseController::class, 'index'])->name('courses.index');
 Route::get('courses/{course}',[CourseController::class, 'show'])->name('courses.show');
 Route::post('course/{course}/approved', [CourseController::class, 'approved'])->name('courses.approved');
