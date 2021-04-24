13352f41d9b2e2ef7c27e942bc5838114818e42f
CesarSG <cesar_sevilla@outlook.com>
2021-04-23 20:05:36 -0500

    Fx Front Courses

--- resources/views/livewire/courses-index.blade.php
+++ resources/views/livewire/courses-index.blade.php
@@ -1,5 +1,5 @@
 <div>    
-    <div class="bg-gray-200 py-4 mb-16">
+    <div class="bg-gray-200 py-10 mb-16">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex">
             <button class="bg-white shadow h-12 px-4 rounded-lg text-gray-700 mr-4 focus:outline-none" wire:click="resetFilters">
                 <i class="fas fa-stream text-xs mr-2"></i> Todos los cursos
@@ -40,7 +40,7 @@
     </div>
 
 
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
+    <div class="max-w-7xl mx-auto px-4 pb-10 sm:px-6 lg:px-8 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
         @foreach ($courses as $course)
             <x-course-card :course="$course"/>
         @endforeach
