4e8918a8d19660532c01e679a6a9b5060a9024b6
CesarSG <cesar_sevilla@outlook.com>
2021-05-12 15:48:07 -0500

    Fx Index Courses

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -5,7 +5,7 @@
         <div class="container py-10 lg:py-36">
             <div class="grid gird-rows-1 grid-cols-1 lg:grid-cols-2">
                 <div class="lg:pl-16 xl:pl-24 lg:pr-28 box-image">
-                    <img class="inline" src="{{ asset('img/home/aprende.svg')}}" alt="...">
+                    <img class="inline" src="{{ asset('img/home/busca.svg')}}" alt="...">
                 </div>
                 <div class="grid gird-rows-3 grid-cols-1 md:mx-20 lg:mx-0 lg:mr-28 box-center">
                     <div>   
