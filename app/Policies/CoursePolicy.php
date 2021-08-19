0df8f5da7bd0466aeeb19b4118b681a88c747ca8
kira187 <missael133@gmail.com>
2021-08-19 21:01:47 +0000

    Hf: Changed correctly name to model Review

--- app/Policies/CoursePolicy.php
+++ app/Policies/CoursePolicy.php
@@ -4,7 +4,7 @@ namespace App\Policies;
 
 use App\Models\User;
 use App\Models\course;
-use App\Models\review;
+use App\Models\Review;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class CoursePolicy
