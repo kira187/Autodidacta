f52a598d2f2bd0e4855322a31d6d11fc81112972
Misael Aguas Jimenez <missael133@gmail.com>
2021-08-29 19:36:44 -0500

    Corrections to laravel psr standar

--- app/Models/Price.php
+++ app/Models/Price.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class price extends Model
+class Price extends Model
 {
     use HasFactory;
 
