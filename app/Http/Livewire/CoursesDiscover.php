2514112a28775a8de030a5049d3fd7e54dff47c8
Cesar <cesar_sevilla@outlook.com>
2021-10-12 20:22:40 -0500

    Show Recommendations: show recommendations in discover

--- app/Http/Livewire/CoursesDiscover.php
+++ app/Http/Livewire/CoursesDiscover.php
@@ -53,7 +53,7 @@ class CoursesDiscover extends Component
             ->limit(15)->get();
 
         $courses_math = Course::whereStatus(3)
-            ->category(5)
+            ->category(10)
             ->level($this->level_id)
             ->orderBy('students_count', 'desc')
             ->limit(15)->get();
@@ -63,11 +63,57 @@ class CoursesDiscover extends Component
         // Check if the user is logged
         if(Auth::check()){
 
-            $courses_view = Course::whereHas('students', function($query){
-                $query->where('user_id', auth()->user()->id);
-            })->get();
+            // Get the host from the AI
+            $hostname = env("AI_HOST", false);
 
-            return view('livewire.courses-discover', compact('courses', 'courses_programming', 'courses_view', 'courses_web_development', 'courses_design', 'courses_electronic', 'courses_math'));
+            if($hostname !== false){
+
+                // Get all the courses from the current user
+                $courses_current_user = Course::whereHas('students', function($query){
+                    $query->where('user_id', auth()->user()->id);
+                })->get();
+
+                $idsUserCourses = $courses_current_user->pluck('id')->toArray();
+
+                $response = Http::get($hostname.auth()->user()->id);
+
+                // Verify if the request is succesful
+                if($response->successful()){
+
+                    $idUsers = $response->json();
+                    $coursesToRecomend = array();
+
+                    //dd($idUsers);
+
+                    // Loop through ids of users recommended
+                    foreach($idUsers as $id){
+
+                        $courses_recommendation = Course::whereHas('students', function($query) use($id){
+                            $query->where('user_id', $id);
+                        })->get();
+
+                        if($courses_recommendation->isNotEmpty()){
+
+                            $idsCourses = $courses_recommendation->pluck('id')->toArray(); // get only the id of the courses
+
+                            $resultado = array_diff($idsCourses, $idsUserCourses); // get the different courses for the recomendation
+                            
+                            $coursesToRecomend = array_merge($coursesToRecomend, $resultado); // get the results to the array
+                        }
+
+                    }
+
+                    if(!empty($coursesToRecomend)){
+
+                        $courses_recommend = Course::whereIn('id', $coursesToRecomend)->get(); 
+
+                        return view('livewire.courses-discover', compact('courses', 'courses_programming', 'courses_recommend', 'courses_web_development', 'courses_design', 'courses_electronic', 'courses_math'));
+                    }
+
+                    
+                }
+            }
+
         }
 
         return view('livewire.courses-discover', compact('courses', 'courses_programming', 'courses_web_development', 'courses_design', 'courses_electronic', 'courses_math'));
