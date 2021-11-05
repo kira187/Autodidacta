fb30e8f7a997c834d57e7687ac5bf423db521754
Misael Aguas Jimenez <missael133@gmail.com>
2021-11-04 22:41:53 -0600

    Hf: chat redirections

--- resources/views/livewire/chat/message-input.blade.php
+++ resources/views/livewire/chat/message-input.blade.php
@@ -1,6 +1,6 @@
 <div class="chat-app-form">
     <div class="input-group input-group-merge mr-1 form-send-message">
-        <input wire:model="text" wire:keydown.enter="sendMessage" type="text"  id="message" class="form-control message {{ ($errors->has('text') ? 'is-invalid' : '')  }}" placeholder="Escribe un mensaje" />
+        <input wire:model.debounce.500ms="text" wire:keydown.enter="sendMessage" type="text"  id="message" class="form-control message {{ ($errors->has('text') ? 'is-invalid' : '')  }}" placeholder="Escribe un mensaje" />
     </div>
     @if (!empty($text))
         <button type="button" class="btn btn-primary send" wire:click="sendMessage">
